38e9eec348e038edfecf7fc0bed0360b8e94dc9d
unknown <gmandelinha@gmail.com>
2020-05-26 18:03:46 -0300

    encerrado

---
+++ Celular.cs
@@ -0,0 +1,21 @@
+namespace AulaPOOCelular
+{
+    public class Celular
+    {
+        public string cor; 
+
+        public string modelo;
+
+        public int on;
+
+        public string mensagem;
+
+        public int ligar;
+
+        public int tamanho;
+
+     
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,51 @@ namespace AulaPOOCelular
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Celular smartphone = new Celular();
+
+
+        Console.WriteLine("Se deseja ligar o dispositivo aperte 1, e 2 para desliagar:");
+        smartphone.on = int.Parse(Console.ReadLine());
+            
+        switch (smartphone.on)
+        {
+
+            case 1:Console.WriteLine("Celular ligado");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Digite a cor de seu celular:");
+            smartphone.cor  = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Digite o modelo do seu celular:");
+            smartphone.modelo  = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+
+
+            Console.WriteLine("Qual o tamanho do seu celular?");
+            smartphone.tamanho  = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Digite a mensagem desejada:");
+            smartphone.mensagem  = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Mensagem enviada!!!");
+
+            Console.WriteLine("Disque o numero em que deseja ligar:");
+            smartphone.ligar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ligação realizada!!!");
+            Console.WriteLine("-------------------------------");
+    
+            break;
+            
+            case 2:Console.WriteLine("Celular desligado");
+            break;
+            
+            
         }
+
+           } 
     }
 }
